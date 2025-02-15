d3518b7a1643563a73412176b492cf4b962874dd
Parthasarathy Sastry <parthasarathy_sastry@yahoo.com>
2025-02-15 09:06:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,4 @@ if prompt:=st.chat_input(placeholder="What is Machine Learning"):
         response=search_agent.run(st.session_state.messages, callbacks=[st_cb])
         st.session_state.messages.append({"role": "assistant", "content": response})
         st.write(response)
+
